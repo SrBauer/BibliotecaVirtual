1024dd5fdedc5b08e06a8dbd5db880814133bf3c
SrBauer <bauer.jonathan.jj@gmail.com>
2024-05-07 20:44:35 -0300

    Create Contexto

---
+++ BibliotecaVirtual/Contexto/ContextDBApplication.cs
@@ -0,0 +1,13 @@
+﻿using BibliotecaVirtu.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaVirtual.Contexto
+{
+    public class ContextDBApplication : DbContext
+    {
+        public ContextDBApplication(DbContextOptions<ContextDBApplication> options) : base(options)
+        {
+        }
+        public DbSet<LivroDTO> Livros { get; set; }
+    }
+}
